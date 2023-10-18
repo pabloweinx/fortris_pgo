c078571ab15d3d286922f6b4dc5d63a821ef9e56
Pablo García <development@musicmonsta.com>
2023-10-18 23:43:25 +0200

    Removed unused imports

--- frontend/src/app/components/accounts/accounts.module.ts
+++ frontend/src/app/components/accounts/accounts.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { AccountsListPageComponent } from './accounts-list-page/accounts-list-page.component';
 import { AccountDetailsPageComponent } from './account-details-page/account-details-page.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [

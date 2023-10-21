ab8a7eabaefd43b8733d812f04ca6c2d2ac2a875
Pablo García <development@musicmonsta.com>
2023-10-21 15:27:57 +0200

    Some typing

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Account } from '../interfaces/account';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,8 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getAccounts() {
-    return this.httpClient.get(environment.API_URL + '/accounts');
+  getAccounts(): Observable<Account[]> {
+    return this.httpClient.get<Account[]>(environment.API_URL + '/accounts');
   }
 
   getAccount(id: number) {

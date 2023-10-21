f001412d8392742f675507328fa789310fa87cc8
Pablo García <development@musicmonsta.com>
2023-10-21 15:27:16 +0200

    Added huge code to socket front service

--- frontend/src/app/services/socket.service.ts
+++ frontend/src/app/services/socket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import * as io from 'socket.io-client';
+import { Account } from '../interfaces/account';
 
 @Injectable({
   providedIn: 'root'
@@ -8,27 +9,58 @@ import * as io from 'socket.io-client';
 export class SocketService {
   private socket: io.Socket;
 
+  private accountsDataSubject = new BehaviorSubject<Account[]>([]);
+  accountsData$ = this.accountsDataSubject.asObservable();
+
+  private accountBalanceUpdateSubject = new BehaviorSubject<Account[]>([]);
+  accountBalanceUpdate$ = this.accountBalanceUpdateSubject.asObservable();
+
+  private exchangeRateUpdateSubject = new BehaviorSubject<number>(0);
+  exchangeRateUpdate$ = this.exchangeRateUpdateSubject.asObservable();
+
   constructor() {
     this.socket = io.connect('http://localhost:3000');
-
-    this.socket.on("connect", () => {
-      console.log('connected to ws!');
-    });
-
-    this.socket.on("disconnect", () => {
-      console.log('disconnected from ws.');
-    });
+    this.socket.on("connect", () => console.log('connected to ws!'));
+    this.socket.on('newExchangeRate', newExchangeRate => this.onExchangeRateUpdate(newExchangeRate));
+    this.socket.on('accountBalanceUpdate', updatedAccount => this.onAccountBalanceUpdate(updatedAccount));
+    this.socket.on("disconnect", () => console.log('disconnected from ws.'));
   }
 
-  getExchangeRate(): Observable<number> {
-    return new Observable(observer => {
-      this.socket.on('newExchangeRate', (rate: number) => {
-        observer.next(rate);
-      });
-    });
+  onExchangeRateUpdate(newExchangeRate: number) {
+    this.exchangeRateUpdateSubject.next(newExchangeRate);
   }
 
-  disconnect(){
+  onAccountBalanceUpdate(updatedAccount: Account) {
+    // Aquí recibes una sola cuenta actualizada, la gestionas y la añades a accountsStateUpdates$
+    const currentUpdates = this.accountBalanceUpdateSubject.getValue();
+    const indexToUpdate = currentUpdates.findIndex(account => account._id === updatedAccount._id);
+
+    if (indexToUpdate !== -1) {
+      currentUpdates[indexToUpdate] = updatedAccount;
+    } else {
+      currentUpdates.push(updatedAccount);
+    }
+
+    this.accountBalanceUpdateSubject.next(currentUpdates);
+  }
+
+  /**
+   * This method is needed to feed the behaviour subject of the accounts to work with them more efficiently
+   * @param accounts the array of accounts
+   */
+  setInitialAccounts(accounts: Account[]): void {
+    this.accountsDataSubject.next(accounts);
+  }
+
+  /**
+   * This method is needed to feed the behaviour subject of the exchange rate
+   * @param rate the initial exchange rate value
+   */
+  setInitialExchangeRate(rate: number){
+    this.exchangeRateUpdateSubject.next(rate);
+  }
+
+  disconnect() {
     this.socket.disconnect();
   }
 }

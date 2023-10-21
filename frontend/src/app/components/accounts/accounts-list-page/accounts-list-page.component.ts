7dae446e026a7ab0e194edd458a2ba2b7504be76
Pablo García <development@musicmonsta.com>
2023-10-21 15:27:48 +0200

    Added all the rxjs needed in account list page

--- frontend/src/app/components/accounts/accounts-list-page/accounts-list-page.component.ts
+++ frontend/src/app/components/accounts/accounts-list-page/accounts-list-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, filter, map, switchMap } from 'rxjs';
+import { Observable, combineLatest, combineLatestWith, filter, forkJoin, map, switchMap, tap } from 'rxjs';
 import { Account } from 'src/app/interfaces/account';
 import { ApiService } from 'src/app/services/api.service';
 import { SocketService } from 'src/app/services/socket.service';
@@ -12,6 +12,9 @@ import { SocketService } from 'src/app/services/socket.service';
 export class AccountsListPageComponent implements OnInit, OnDestroy {
 
   accounts$!: Observable<Account[]>;
+  accountsData$: Observable<Account[]> = this.socketService.accountsData$;
+  accountBalanceUpdate$: Observable<Account[]> = this.socketService.accountBalanceUpdate$;
+  exchangeRateUpdate$: Observable<number> = this.socketService.exchangeRateUpdate$;
 
   constructor(
     private apiService: ApiService,
@@ -20,18 +23,54 @@ export class AccountsListPageComponent implements OnInit, OnDestroy {
 
   }
   ngOnInit(): void {
-    this.accounts$ = this.socketService.getExchangeRate().pipe(
+
+    // We'll store the db accounts to work with them when receiving socket updates
+    this.setInitialData();
+
+    // Preventing this observable to participate in the combineLatest op till it has a valid value (e. g, the one obtained in the setInitialData())
+    this.exchangeRateUpdate$.pipe(
+      filter(rate => rate !== 0)
+    )
+
+    this.accounts$ = combineLatest([this.accountsData$, this.accountBalanceUpdate$, this.exchangeRateUpdate$]).pipe(
+      map(([accountsData, accountBalanceUpdate, exchangeRateUpdate]) => {
+        // First, we apply the Exchange Rate to the accounts
+        const accountsWithUpdatedExchangeRate = accountsData.map((account: Account) => {
+          const updatedBalance = account.balance * exchangeRateUpdate;
+          return { ...account, balance: updatedBalance };
+        });
+
+        // Then, we apply balance update and recalculate everything
+        return accountsWithUpdatedExchangeRate.map((account: Account) => {
+          const update = accountBalanceUpdate.find(updatedAccount => updatedAccount._id === account._id);
+          return update ? this.transformAccount({ ...account, ...update }, exchangeRateUpdate) : account;
+        });        
+      })
+    );
+  }
+
+  /**
+   * This method makes a first request for getting the value of the current exchange rate so then
+   * we can get the accounts with all its fields calculated. It adds more stability and freshness
+   * since the beginning of the app execution.
+   */
+  setInitialData() {
+    this.apiService.getExchangeRate().pipe(
+      tap(rate => this.socketService.setInitialExchangeRate(rate)),
       switchMap(
         rate => this.apiService.getAccounts().pipe(
           map((accounts: any) =>
-            accounts.map((account: Account) => this.transformObject(account, rate))
+            accounts.map((account: Account) => this.transformAccount(account, rate)
+            )
           )
         )
       )
-    )
+    ).subscribe(accounts => {
+      this.socketService.setInitialAccounts(accounts);
+    });
   }
 
-  transformObject(account: Account, rate: number) {
+  transformAccount(account: Account, rate: number) {
     return {
       ...account,
       balance_btc: account.balance,

bc77ce7809b9f6d1d3bfe5a0bb60a86d61a332b0
Pablo García <development@musicmonsta.com>
2023-10-21 15:21:00 +0200

    Configured cors for the websocket

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
+  app.use(cors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+  }));
   await app.listen(3000);
 }
 bootstrap();

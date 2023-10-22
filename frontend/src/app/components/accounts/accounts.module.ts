da57b266580f43f193c0f03e0d674b7b0c43d411
Pablo García <development@musicmonsta.com>
2023-10-23 00:23:47 +0200

    Bringing Angular Material to our GUI

--- frontend/src/app/components/accounts/accounts.module.ts
+++ frontend/src/app/components/accounts/accounts.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { AccountsListPageComponent } from './accounts-list-page/accounts-list-page.component';
 import { AccountDetailsPageComponent } from './account-details-page/account-details-page.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule  } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -11,7 +16,12 @@ import { AccountDetailsPageComponent } from './account-details-page/account-deta
   ],
   imports: [
     CommonModule,
-    AccountsRoutingModule
+    AccountsRoutingModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class AccountsModule { }

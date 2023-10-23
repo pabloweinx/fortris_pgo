51e01893c839cd63719b2a80c913d80d64db0aae
Pablo García <development@musicmonsta.com>
2023-10-24 00:26:57 +0200

    Pure reactiveness on events from api and sockets together

--- frontend/src/app/services/account.service.ts
+++ frontend/src/app/services/account.service.ts
@@ -11,24 +11,13 @@ export class AccountService {
   private _accountsSubject = new BehaviorSubject<Account[]>([]);
   public accounts$ = this._accountsSubject.asObservable();
 
-  private _accountsDataSubject = new BehaviorSubject<Account[]>([]);
-  public accountsData$ = this._accountsDataSubject.asObservable();
-
-  private _accountBalanceUpdateSubject = new BehaviorSubject<Account[]>([]);
+  private _accountBalanceUpdateSubject = new BehaviorSubject<Account | null>(null);
   public accountBalanceUpdate$ = this._accountBalanceUpdateSubject.asObservable();
 
   private _selectedAccountSubject = new BehaviorSubject<Account | null>(null);
   public selectedAccount$ = this._selectedAccountSubject.asObservable();
 
   constructor(private apiService: ApiService) { }
-
-  /**
-   * This method is needed to feed the behaviour subject of the accounts to work with them more efficiently
-   * @param accounts the array of accounts
-   */
-  setInitialAccounts(accounts: Account[]): void {
-    this._accountsDataSubject.next(accounts);
-  }
 
   setSelectedAccount(account: Account): void {
     this._selectedAccountSubject.next(account);
@@ -38,11 +27,9 @@ export class AccountService {
     this._accountsSubject.next(newData);
   }
 
-  transformAccount(account: Account, rate: number) {
+  transformAccount(account: Account, rate: number): Account {
     return {
       ...account,
-      balance_btc: account.balance,
-      available_balance_btc: account.available_balance,
       // We don't need to store in the db the values calculated in dollars
       balance_dollar: account.balance * rate,
       available_balance_dollar: account.available_balance * rate
@@ -50,64 +37,34 @@ export class AccountService {
   }
 
   onAccountBalanceUpdate(updatedAccount: Account) {
-    // Obtienes el valor actual del Subject
-    const currentUpdates = this._accountBalanceUpdateSubject.getValue();
-
-    // 1. Eliminas el atributo 'state' de todas las cuentas en currentUpdates
-    currentUpdates.forEach(account => {
-      delete account.state;
-    });
-
-    // 2. Buscas si la cuenta actualizada ya existe en currentUpdates
-    const indexToUpdate = currentUpdates.findIndex(account => account._id === updatedAccount._id);
-
-    if (indexToUpdate !== -1) {
-      // Si ya existe, actualizas la cuenta con la nueva información
-      currentUpdates[indexToUpdate] = updatedAccount;
-    } else {
-      // Si no existe, la añades al final de currentUpdates
-      currentUpdates.push(updatedAccount);
-    }
-
-    // Emite el array actualizado a través del Subject
-    this._accountBalanceUpdateSubject.next(currentUpdates);
+    this._accountBalanceUpdateSubject.next(updatedAccount);
   }
 
-  resetStates() {
-    const removeStateAttribute = (accounts: Account[]): Account[] => {
-      return accounts.map(account => {
-        const { state, ...rest } = account;
-        return rest;
-      });
-    };
+  getCurrentSelectedAccount(): Account | null {
+    return this._selectedAccountSubject.getValue();
+  }
 
-    const currentAccounts = this._accountsSubject.getValue();
-    const processedAccounts = removeStateAttribute(currentAccounts);
-    this._accountsSubject.next(processedAccounts);
-
-    const currentAccountsData = this._accountsDataSubject.getValue();
-    const processedAccountsData = removeStateAttribute(currentAccountsData);
-    this._accountsDataSubject.next(processedAccountsData);
-
-    console.log(this._accountsSubject.getValue());
-    console.log(this._accountsDataSubject.getValue());
+  resetState() {
+    const currentBalanceUpdateAccount = this._accountBalanceUpdateSubject.getValue();
+    delete currentBalanceUpdateAccount?.state;
+    this._accountBalanceUpdateSubject.next(currentBalanceUpdateAccount);
   }
 
   getAccountDetail(accountId: string): Observable<Account> {
-    const existingAccount = this._accountsDataSubject.getValue().find(account => account._id === accountId);
+    const existingAccount = this._accountsSubject.getValue().find(account => account._id === accountId);
 
     // Regardless of whether the Account already exists in the Behaviorsubject, we need to obtain the most recent details.
     return this.apiService.getAccount(accountId).pipe(
       tap(account => {
         if (existingAccount) {
           // Update the account at the Behaviorsubject if it already exists.
-          const index = this._accountsDataSubject.getValue().indexOf(existingAccount);
-          const updatedAccounts = [...this._accountsDataSubject.getValue()];
+          const index = this._accountsSubject.getValue().indexOf(existingAccount);
+          const updatedAccounts = [...this._accountsSubject.getValue()];
           updatedAccounts[index] = account;
-          this._accountsDataSubject.next(updatedAccounts);
+          this._accountsSubject.next(updatedAccounts);
         } else {
           // or add the new account to the Behaviorsubject if it did not exist.
-          this._accountsDataSubject.next([...this._accountsDataSubject.getValue(), account]);
+          this._accountsSubject.next([...this._accountsSubject.getValue(), account]);
         }
         // In both cases, we update the Behaviorsubject that stores the selected account.
         this._selectedAccountSubject.next(account);
--- frontend/src/app/services/reactive.service.ts
+++ frontend/src/app/services/reactive.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, combineLatest, map, shareReplay } from 'rxjs';
+import { Observable, Subject, combineLatest, filter, map, shareReplay, tap } from 'rxjs';
 import * as io from 'socket.io-client';
 import { Account } from '../interfaces/account.interface';
 import { AccountService } from './account.service';
@@ -11,13 +11,12 @@ import { ExchangeRateService } from './exchange-rate.service';
 export class ReactiveService {
   private socket!: io.Socket;
 
-  private combinedData$!: Observable<Account[]>;
-  private combinedSelectedAccountData$!: Observable<Account>;
-
   constructor(
     private accountService: AccountService,
     private exchangeRateService: ExchangeRateService
-  ) { }
+  ) {
+    this.initReactivity();
+  }
 
   initReactivity() {
     this.socket = io.connect('http://localhost:3000');
@@ -25,50 +24,6 @@ export class ReactiveService {
     this.socket.on('newExchangeRate', newExchangeRate => this.exchangeRateService.onExchangeRateUpdate(newExchangeRate));
     this.socket.on('accountBalanceUpdate', updatedAccount => this.accountService.onAccountBalanceUpdate(updatedAccount));
     this.socket.on("disconnect", () => console.log('disconnected from ws.'));
-
-    // The reactive process for the accounts list page
-    this.combinedData$ = combineLatest([this.accountService.accountsData$, this.accountService.accountBalanceUpdate$, this.exchangeRateService.exchangeRateUpdate$]).pipe(
-      map(([accountsData, accountBalanceUpdate, exchangeRateUpdate]) => {
-        // First, we apply the Exchange Rate to the accounts
-        const accountsWithUpdatedExchangeRate = accountsData.map((account: Account) => {
-          const updatedBalance = account.balance * exchangeRateUpdate;
-          return { ...account, balance: updatedBalance };
-        });
-
-        // Then, we apply balance update and recalculate everything
-        return accountsWithUpdatedExchangeRate.map((account: Account) => {
-          const update = accountBalanceUpdate.find(updatedAccount => updatedAccount._id === account._id);
-          return update ? this.accountService.transformAccount({ ...account, ...update }, exchangeRateUpdate) : account;
-        });
-      }),
-      shareReplay(1)
-    );
-
-    this.combinedData$.subscribe(data => {
-      this.accountService.updateAccounts(data);
-    });
-
-    // The reactive process for the account detils page
-    this.combinedSelectedAccountData$ = combineLatest([
-      this.accountService.selectedAccount$,
-      this.accountService.accountBalanceUpdate$,
-      this.exchangeRateService.exchangeRateUpdate$
-    ]).pipe(
-      map(([selectedAccount, accountBalanceUpdate, exchangeRateUpdate]) => {
-        const update = accountBalanceUpdate.find(update => update._id === (selectedAccount as Account)._id);
-        if (update) {
-          selectedAccount = this.accountService.transformAccount({
-            ...(selectedAccount as Account),
-            balance: update.balance ? update.balance : (selectedAccount as Account).balance,
-            available_balance: update.available_balance ? update.available_balance : (selectedAccount as Account).available_balance,
-          }, exchangeRateUpdate);
-        } else {
-          selectedAccount = this.accountService.transformAccount((selectedAccount as Account), exchangeRateUpdate);
-        }
-
-        return selectedAccount;
-      })
-    );
   }
 
   disconnect() {

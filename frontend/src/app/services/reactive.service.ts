446f671b295a80a82ee395611d737106ec50a130
Pablo García <development@musicmonsta.com>
2023-10-24 16:06:40 +0200

    Removing unused imports

--- frontend/src/app/services/reactive.service.ts
+++ frontend/src/app/services/reactive.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject, combineLatest, filter, map, shareReplay, tap } from 'rxjs';
 import * as io from 'socket.io-client';
-import { Account } from '../interfaces/account.interface';
-import { AccountService } from './account.service';
+import { AccountsService } from './accounts.service';
 import { ExchangeRateService } from './exchange-rate.service';
 
 @Injectable({
@@ -12,17 +10,20 @@ export class ReactiveService {
   private socket!: io.Socket;
 
   constructor(
-    private accountService: AccountService,
+    private accountsService: AccountsService,
     private exchangeRateService: ExchangeRateService
   ) {
     this.initReactivity();
   }
 
+/**
+ * This method connects to all WebSocket events that we need from the server
+ */
   initReactivity() {
     this.socket = io.connect('http://localhost:3000');
     this.socket.on("connect", () => console.log('connected to ws!'));
     this.socket.on('newExchangeRate', newExchangeRate => this.exchangeRateService.onExchangeRateUpdate(newExchangeRate));
-    this.socket.on('accountBalanceUpdate', updatedAccount => this.accountService.onAccountBalanceUpdate(updatedAccount));
+    this.socket.on('accountBalanceUpdate', updatedAccount => this.accountsService.onAccountBalanceUpdate(updatedAccount));
     this.socket.on("disconnect", () => console.log('disconnected from ws.'));
   }
 

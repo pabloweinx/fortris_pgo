25a544fdcf9d5ed2dba7426f173996a675e13ff5
Pablo García <development@musicmonsta.com>
2023-10-24 16:05:17 +0200

    Set the name of the project

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Senior developer test - Pablo García Ojeda';
 }
